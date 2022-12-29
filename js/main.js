bf1d1efcba075d6af2946379a8ad128a6532b1e5
Asit Biswas <asitbiswas870@gmail.com>
2022-12-29 15:18:30 +0530

    add about page

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 const menu = document.querySelector(".menu");
 const sideBar = document.querySelector(".side-bar");
 const x = document.querySelector(".x");
+const container = document.querySelector(".blur");
 menu.addEventListener("click", () => {
     sideBar.style.transform = "translateX(0)";
+    menu.style.display = "none";
 });
 x.addEventListener("click", () => {
     sideBar.style.transform = "translateX(100%)";
+    menu.style.display = "block";
 });
--- typescript/main.ts
+++ typescript/main.ts
@@ -8,9 +8,12 @@
 const menu = document.querySelector(".menu")! as HTMLDivElement;
 const sideBar = document.querySelector(".side-bar")! as HTMLDivElement;
 const x = document.querySelector(".x")! as HTMLElement;
+const container = document.querySelector<HTMLDivElement>(".blur")!;
 menu.addEventListener("click",()=>{
     sideBar.style.transform = "translateX(0)";
+    menu.style.display = "none";
 })
 x.addEventListener("click",()=>{
     sideBar.style.transform = "translateX(100%)";
+    menu.style.display = "block";
 })
