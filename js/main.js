215eecb1ed617a6b6f269a4c36ddff6b92973ba6
Asit Biswas <asitbiswas870@gmail.com>
2022-12-31 14:04:00 +0530

    add btn click event

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const menu = document.querySelector(".menu");
 const sideBar = document.querySelector(".side-bar");
 const x = document.querySelector(".x");
 const container = document.querySelector(".blur");
+const btn = document.querySelector("button");
+const form = document.querySelector("form");
 menu.addEventListener("click", () => {
     sideBar.style.transform = "translateX(0)";
     menu.style.display = "none";
@@ -10,4 +12,8 @@ menu.addEventListener("click", () => {
 x.addEventListener("click", () => {
     sideBar.style.transform = "translateX(100%)";
     menu.style.display = "block";
+});
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    form?.reset();
 });
--- typescript/main.ts
+++ typescript/main.ts
@@ -9,6 +9,8 @@ const menu = document.querySelector(".menu")! as HTMLDivElement;
 const sideBar = document.querySelector(".side-bar")! as HTMLDivElement;
 const x = document.querySelector(".x")! as HTMLElement;
 const container = document.querySelector<HTMLDivElement>(".blur")!;
+const btn = document.querySelector<HTMLButtonElement>("button")!;
+const form = document.querySelector<HTMLFormElement>("form");
 menu.addEventListener("click",()=>{
     sideBar.style.transform = "translateX(0)";
     menu.style.display = "none";
@@ -17,3 +19,7 @@ x.addEventListener("click",()=>{
     sideBar.style.transform = "translateX(100%)";
     menu.style.display = "block";
 })
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    form?.reset();
+});
