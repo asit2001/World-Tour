0895d69d38140cb207b996bf2368f6014e0fa19c
Asit Biswas <asitbiswas870@gmail.com>
2023-01-01 11:21:56 +0530

    handel if elements not present

--- js/main.js
+++ js/main.js
@@ -8,24 +8,24 @@ const form = document.querySelector("form");
 const logInSignUpFrom = document.querySelectorAll(".sign");
 const signUp = document.querySelector("#signUp");
 const logIn = document.querySelector("#logIn");
-menu.addEventListener("click", () => {
+menu?.addEventListener("click", () => {
     sideBar.style.transform = "translateX(0)";
     menu.style.display = "none";
 });
-x.addEventListener("click", () => {
+x?.addEventListener("click", () => {
     sideBar.style.transform = "translateX(100%)";
     menu.style.display = "block";
 });
-btn.addEventListener("click", (e) => {
+btn?.addEventListener("click", (e) => {
     e.preventDefault();
     form?.reset();
 });
-logIn.addEventListener("click", (e) => {
+logIn?.addEventListener("click", (e) => {
     e.preventDefault();
     logInSignUpFrom[0].style.display = "none";
     logInSignUpFrom[1].style.display = "flex";
 });
-signUp.addEventListener("click", (e) => {
+signUp?.addEventListener("click", (e) => {
     e.preventDefault();
     logInSignUpFrom[1].style.display = "none";
     logInSignUpFrom[0].style.display = "flex";
--- typescript/main.ts
+++ typescript/main.ts
@@ -5,34 +5,34 @@
 //     changeImages()
 //     i++;
 // }, 5000);
-const menu = document.querySelector(".menu span")! as HTMLDivElement;
-const sideBar = document.querySelector(".side-bar")! as HTMLDivElement;
-const x = document.querySelector(".x")! as HTMLElement;
-const container = document.querySelector<HTMLDivElement>(".blur")!;
-const btn = document.querySelector<HTMLButtonElement>("button")!;
+const menu = document.querySelector(".menu span") as HTMLDivElement;
+const sideBar = document.querySelector(".side-bar") as HTMLDivElement;
+const x = document.querySelector(".x") as HTMLElement;
+const container = document.querySelector<HTMLDivElement>(".blur");
+const btn = document.querySelector<HTMLButtonElement>("button");
 const form = document.querySelector<HTMLFormElement>("form");
-const logInSignUpFrom = document.querySelectorAll<HTMLFormElement>(".sign")!;
-const signUp = document.querySelector<HTMLAnchorElement>("#signUp")!;
-const logIn = document.querySelector<HTMLAnchorElement>("#logIn")!;
+const logInSignUpFrom = document.querySelectorAll<HTMLFormElement>(".sign");
+const signUp = document.querySelector<HTMLAnchorElement>("#signUp");
+const logIn = document.querySelector<HTMLAnchorElement>("#logIn");
 
-menu.addEventListener("click",()=>{
+menu?.addEventListener("click",()=>{
     sideBar.style.transform = "translateX(0)";
     menu.style.display = "none";
 })
-x.addEventListener("click",()=>{
+x?.addEventListener("click",()=>{
     sideBar.style.transform = "translateX(100%)";
     menu.style.display = "block";
 })
-btn.addEventListener("click",(e)=>{
+btn?.addEventListener("click",(e)=>{
     e.preventDefault();
     form?.reset();
 });
-logIn.addEventListener("click",(e)=>{
+logIn?.addEventListener("click",(e)=>{
     e.preventDefault();
     logInSignUpFrom[0].style.display = "none";
     logInSignUpFrom[1].style.display = "flex";
 })
-signUp.addEventListener("click",(e)=>{
+signUp?.addEventListener("click",(e)=>{
     e.preventDefault();
     logInSignUpFrom[1].style.display = "none";
     logInSignUpFrom[0].style.display = "flex";
